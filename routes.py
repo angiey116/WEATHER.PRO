0cdbeb476fb7f41d701ad4f29ae306af88f76107
angiey116 <angiey116@yahoo.com>
2026-03-06 09:30:21 -0600

    updated CRUD routes

--- routes.py
+++ routes.py
@@ -1,27 +1,23 @@
-# routes.py
-
 def register_routes(app):
-
     @app.get("/")
     def home():
         return "<h1>milestone 2</h1><p>Flask server is running.</p>"
 
     @app.post("/items")
     def create_item():
-        return "CREATE: POST /items"
+        return "create items"
 
     @app.get("/items")
     def list_items():
-        return "READ ALL: GET /items"
+        return "read all items"
 
     @app.get("/items/<int:item_id>")
     def get_item(item_id):
-        return f"READ ONE: GET /items/{item_id}"
+        return f"read one: get /items"
 
     @app.put("/items/<int:item_id>")
     def update_item(item_id):
-        return f"UPDATE: PUT /items/{item_id}"
-
+        return f"update items"
     @app.delete("/items/<int:item_id>")
     def delete_item(item_id):
-        return f"DELETE: DELETE /items/{item_id}"
+        return f"delete items"
